b70c00841d916e28ecae6c49d29039f0e21249f4
suresh1 <suresh1@gmail.com>
2026-03-01 19:37:58 +0530

    updated version

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('success/', views.success, name='success'),
-    path('history/', views.history, name='history'),
-    path('account/', views.account, name='account'),
-    path('contact/', views.contact, name='contact'),
-    path('signup/', views.signup, name='signup'),
-    path('accounts/', include('django.contrib.auth.urls')), # Built-in login/logout
+    path('history/', views.history, name='history'),   # My Bookings page
+    path('account/', views.account, name='account'),   # Account page
+    path('about/', views.about, name='about'),         # About page
+    path('contact/', views.contact, name='contact'),   # Contact page
+    path('signup/', views.signup, name='signup'),     # Signup page
 ]
